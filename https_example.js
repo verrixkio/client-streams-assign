430e52cd05c286b77e6d51bd18f343c4ef548fd7
verrixkio <verrixkio@gmail.com>
2019-02-10 17:59:59 +0000

    checking the incoming message

--- https_example.js
+++ https_example.js
@@ -8,12 +8,14 @@ var options = {
 };
 
 //called by https whn the request is made.
-var callback = function() {
-  console.log('In response handler callback!')
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log('Im about to make the request');
 
 https.request(options, callback).end()
 
-console.log("I've made th request!")
+console.log("I've made the request!")
+
