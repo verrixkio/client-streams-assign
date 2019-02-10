7cd6ffccf8515269c88245cca09124419986fe43
verrixkio <verrixkio@gmail.com>
2019-02-10 17:56:04 +0000

    callback and request test

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
-console.log(https)
+
 console.log('I did it!')
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+//called by https whn the request is made.
+var callback = function() {
+  console.log('In response handler callback!')
+}
+
+console.log('Im about to make the request');
+
+https.request(options, callback).end()
+
+console.log("I've made th request!")
